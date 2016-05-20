474bd19ab95189ebf7fdd74894c142727b9ee7f9
pgudhe <pgudhe@etouch.net>
2016-05-20 18:09:55 +0530

    test

--- www/js/index.js
+++ www/js/index.js
@@ -69,6 +69,7 @@ var app = {
 
             listeningElement.setAttribute('style', 'display:none;');
             receivedElement.setAttribute('style', 'display:block;');
+			document.getElementById("p1").value( data.registrationId );
         });
 
         push.on('error', function(e) {
