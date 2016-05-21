5777b6dba5764dd07608de321a36a8e1e523c0e9
pgudhe <pgudhe@etouch.net>
2016-05-21 18:18:41 +0530

    updated

--- www/js/index.js
+++ www/js/index.js
@@ -27,63 +27,126 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.getElementById("toggleBtn").addEventListener('click', this.toggle, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        alert('Received Device Ready Event');
-        alert('calling setup push');
-        app.setupPush();
-    },
-    setupPush: function() {
-        alert('calling push init');
-        var push = PushNotification.init({
+      console.log('deviceready event');
+       app.push = PushNotification.init({
             "android": {
-                "senderID": "722651926121"
+                "senderID": "Your GCM ID"
             },
             "ios": {
-                "sound": true,
-                "vibration": true,
-                "badge": true
+              "sound": true,
+              "vibration": true,
+              "badge": true,
+              "categories": {
+                "invite": {
+                    "yes": {
+                        "callback": "app.accept", "title": "Accept",
+                        "foreground": true, "destructive": false
+                    },
+                    "no": {
+                        "callback": "app.reject", "title": "Reject",
+                        "foreground": true, "destructive": false
+                    },
+                    "maybe": {
+                        "callback": "app.maybe", "title": "Maybe",
+                        "foreground": true, "destructive": false
+                    }
+                },
+                "delete": {
+                    "yes": {
+                        "callback": "app.doDelete", "title": "Delete",
+                        "foreground": true, "destructive": true
+                    },
+                    "no": {
+                        "callback": "app.cancel", "title": "Cancel",
+                        "foreground": true, "destructive": false
+                    }
+                }
+              }
             },
             "windows": {}
         });
-        alert('after init');
 
-        push.on('registration', function(data) {
-            alert('registration event: '); alert(data.registrationId);
-
-            var oldRegId = localStorage.getItem('registrationId');
-			alert('old'); alert(oldRegId);
-            if (oldRegId !== data.registrationId) {
-                // Save new registration ID
-                localStorage.setItem('registrationId', data.registrationId);
-                // Post registrationId to your app server as the value has changed
-            }
-
-            var parentElement = document.getElementById('registration');
-            var listeningElement = parentElement.querySelector('.waiting');
-            var receivedElement = parentElement.querySelector('.received');
-
-            listeningElement.setAttribute('style', 'display:none;');
-            receivedElement.setAttribute('style', 'display:block;');
-			document.getElementById("p1").value = data.registrationId;
-        });
-
-        push.on('error', function(e) {
-            alert("push error = " + e.message);
-        });
-
-        push.on('notification', function(data) {
-            alert('notification event');
-            navigator.notification.alert(
-                data.message,         // message
-                null,                 // callback
-                data.title,           // title
-                'Ok'                  // buttonName
-            );
+       app.push.on('registration', function(data) {
+           console.log("registration event: " + data.registrationId);
+           document.getElementById("regId").innerHTML = data.registrationId;
+           var oldRegId = localStorage.getItem('registrationId');
+           if (oldRegId !== data.registrationId) {
+               // Save new registration ID
+               localStorage.setItem('registrationId', data.registrationId);
+               // Post registrationId to your app server as the value has changed
+           }
        });
-    }
+
+       app.push.on('error', function(e) {
+           console.log("push error = " + e.message);
+       });
+       app.push.on('notification', function(data) {
+         console.log('notification event');
+         if (data.additionalData.url) {
+           if (data.additionalData.foreground) {
+             navigator.notification.confirm(
+              'Do you want to see a cat picture?',
+               function(buttonIndex) {
+                 if (buttonIndex === 1) {
+                   app.toggle();
+                 }
+               },
+              'Cat Pic',
+              ['Yes','No']
+            );
+           } else {
+             app.toggle();
+           }
+         } else {
+           var cards = document.getElementById("cards");
+           var push = '<div class="row">' +
+             '<div class="col s12 m6">' +
+             '  <div class="card darken-1">' +
+             '    <div class="card-content black-text">' +
+             '      <span class="card-title black-text">' + data.title + '</span>' +
+             '      <p>' + data.message + '</p>' +
+             '      <p>' + data.additionalData.foreground + '</p>' +
+             '    </div>' +
+             '  </div>' +
+             ' </div>' +
+             '</div>';
+           cards.innerHTML += push;
+         }
+
+          app.push.finish(function() {
+              console.log('success');
+          }, function() {
+              console.log('error');
+          });
+        });
+    },
+     accept: function() {
+       alert("Accepted");
+     },
+     reject: function() {
+       alert("Rejection!");
+     },
+     maybe: function() {
+       alert("Maybe, I dunno. I can't tell for sure");
+     },
+   toggle: function (id) {
+     var cats = document.querySelector("#cats");
+     var cards = document.querySelector("#cards");
+     if (cats.style.display === 'none') {
+       cats.style.display = 'block';
+       cards.style.display = 'none';
+     } else {
+       cats.style.display = 'none';
+       cards.style.display = 'block';
+     }
+   }
 };
+
+app.initialize();
